b70ef4ce63927762aea64669da1eb18a31c56172
Serge Balyuk <bgipsy@gmail.com>
2014-11-28 23:17:00 +0200

    Removes error reporting test trap

--- lib/log_aggregator/ingestor_cli.rb
+++ lib/log_aggregator/ingestor_cli.rb
@@ -4,7 +4,6 @@ logger = Logger.new(LogAggregator.root.join('log/errors.log'))
 while line = STDIN.gets
   begin
     line.chomp!
-    boom
     ingestor.handle_input_line(line)
   rescue
     logger.error "Error #{$!.inspect} (while processing '#{line}'):\n#{$!.backtrace.join "\n"}"
