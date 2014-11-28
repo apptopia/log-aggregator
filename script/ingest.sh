a504f47d2ef0bc85c88ec033e9471e9b5c8b847c
Serge Balyuk <bgipsy@gmail.com>
2014-11-28 23:16:24 +0200

    Adds wrapper for rbenv backed envs

---
+++ script/ingest.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rbenv_root="/usr/local/rbenv"
+export RBENV_ROOT="$rbenv_root"
+
+if [ -n "$rbenv_root" ]; then
+  export PATH="${rbenv_root}/bin:$PATH"
+  eval "$(rbenv init -)"
+fi
+
+DIR=$( cd "$( dirname "$0" )" && pwd )
+
+$DIR/ingest
